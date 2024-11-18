ccec8b1118fc8544c68804de45539be69f4dba45
reiamenezes2004 <RM1661@live.mdx.ac.uk>
2024-11-18 10:02:53 +0400

    defining the key movements.

--- teleoperation/turtlesim_teleoperation.py
+++ teleoperation/turtlesim_teleoperation.py
@@ -4,6 +4,15 @@ import rospy
 from geometry_msgs.msg import Twist
 from pynput import keyboard
 
+# initialize variables
+speed = 1.0         # declaring linear speed (m/s)
+turn_speed = 1.0    # declaring angular (rotation) speed (rad/s)
+move_bindings = {
+    'f': (1, 0),  # moves the turtle forward
+    'b': (-1, 0), # moves the turtle backward
+    'l': (0, 1),  # rotates the turtle to the left
+    'r': (0, -1), # rotates the turtle to the right
+}
 
 # initializing the node and publisher
 rospy.init_node('turtlesim_teleoperation', anonymous=True)

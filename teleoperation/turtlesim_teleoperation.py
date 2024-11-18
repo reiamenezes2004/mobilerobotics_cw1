7bde1e7b9e2481f8dad42a4ed8e956262d5a0b7b
reiamenezes2004 <RM1661@live.mdx.ac.uk>
2024-11-18 10:02:08 +0400

    initializing ros node and publisher.

--- teleoperation/turtlesim_teleoperation.py
+++ teleoperation/turtlesim_teleoperation.py
@@ -4,3 +4,10 @@ import rospy
 from geometry_msgs.msg import Twist
 from pynput import keyboard
 
+
+# initializing the node and publisher
+rospy.init_node('turtlesim_teleoperation', anonymous=True)
+pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+rate = rospy.Rate(10)  # 10 Hz
+
+
